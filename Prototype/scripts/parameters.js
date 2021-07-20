6c602c1b14464bb92e7366f91c9ef1a45b4b9260
suitangi <suitangi777@gmail.com>
2021-07-19 21:24:09 -0500

    all

---
+++ Prototype/scripts/parameters.js
@@ -0,0 +1,64 @@
+window.expParam = {
+
+  boxes: [
+    {
+      lower: 5,
+      upper: 10
+    }, {
+      lower: 15,
+      upper: 20
+    }, {
+      lower: 8,
+      upper: 18
+    }, {
+      lower: 7,
+      upper: 49
+    }, {
+      lower: 55,
+      upper: 101
+    },
+  ],
+
+  endFeedbackDuration: 1000,
+  searchCost: 5,
+
+  postquestions: [{
+    question: "What is your age?",
+    placeholder: " ",
+    type: 'number',
+    min: 18,
+    max: 99,
+    title: 'Age'
+  }, {
+    question: "What is your gender?",
+    choices: ["Male", "Female", "Other"],
+    type: 'choice',
+    title: 'Gender'
+  }, {
+    question: "What is your highest level of education?",
+    choices: ["Less than high school", "High school graduate", "Some college", "2 year degree", "4 year degree", "Professional Degree", "Doctorate"],
+    type: 'choice',
+    title: 'Education'
+  }],
+  confirm_popup: {
+    title: "Submission Complete!",
+    content: "Thank you for completing the experiment!"
+  },
+
+  prequestions: [{
+      question: "Consent Question",
+      choices: ["<b>By clicking this button, you are indicating that you are at least 18 years old, have read and understand this consent form, and you agree to participate in this online research study.</b>"],
+      type: 'choice',
+      title: 'Study Information'
+    }, {
+      question: "Welcome to the study! This experiment works the best in the <strong>Chrome browser</strong> with the ad blocker turned off (there are no ads in this experiment). Please read all the instructions carefully. Please work on a <b>laptop or desktop</b> for the study. Please <b>do not</b> exit or refresh the page while you are working on the study.",
+      type: 'text',
+      title: 'Welcome'
+    }, {
+      question: "This sentence is here to give some instrucctions on the experiment that will happen after you click next. This sentence is here to demonstrate what this instruction box will look like as it makes the entire instruction longer.",
+      type: 'text',
+      title: 'Task'
+    },
+  ]
+
+}
